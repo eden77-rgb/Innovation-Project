dc99f3d8192683849279888a1d5bbea9a15e1158
Eden <eden.vandewatyne@estiam.com>
2026-04-28 11:24:35 +0200

    Création des prompts pour la traduction et la réécriture

--- server/app/main.py
+++ server/app/main.py
@@ -1,4 +1,39 @@
 from domain import PromptBuilder, PromptType
 
-prompt = PromptBuilder.build(PromptType.SUMMARY, "aaaaaaaaaaaaaaa")
-print(prompt)
+print(
+    "###########################\n" \
+    "#    PROMPT 1 : Résumé    #\n" \
+    "###########################\n"
+)
+
+prompt = PromptBuilder.build(
+    PromptType.SUMMARY,
+    "L'intelligence artificielle transforme de nombreux secteurs comme la santé, l'éducation et l'industrie. Elle permet d'automatiser des tâches complexes et d'améliorer la prise de décision grâce à l'analyse de grandes quantités de données."
+)
+print(prompt, "\n\n")
+
+
+print(
+    "###########################\n" \
+    "#  PROMPT 2 : Traduction  #\n" \
+    "###########################\n"
+)
+
+prompt = PromptBuilder.build(
+    PromptType.TRANSLATE,
+    "Le changement climatique est un défi majeur pour l'humanité."
+)
+print(prompt, "\n\n")
+
+
+print(
+    "###########################\n" \
+    "#   PROMPT 3 : Réécrire   #\n" \
+    "###########################\n"
+)
+
+prompt = PromptBuilder.build(
+    PromptType.REWRITE,
+    "Je pense que ce produit est bien mais il est un peu cher et pas très pratique."
+)
+print(prompt, "\n\n")
